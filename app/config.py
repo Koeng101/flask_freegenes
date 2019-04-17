f6a14db92681c4ed69f0f15b4f23410d6b053f74
Keoni Gandall <koeng101@gmail.com>
2019-04-16 17:58:12 -0700

    updated config

--- app/config.py
+++ app/config.py
@@ -12,4 +12,4 @@ SPACES = session.client('s3',
                         endpoint_url=os.environ['ENDPOINT_URL'],
                         aws_access_key_id=os.environ['AWS_ACCESS_KEY_ID'],
                         aws_secret_access_key=os.environ['AWS_SECRET_ACCESS_KEY'])
-BUCKET = os.environ['freegenes']
+BUCKET = os.environ['BUCKET']
